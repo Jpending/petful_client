479f9d564b117ea9df2cb25aaa2d6cb94f6e425d
John <jpending@gmail.com>
2020-10-03 11:20:34 -0400

    spellling errors

--- src/App/About.js
+++ src/App/About.js
@@ -7,12 +7,12 @@ export default function About() {
     <div className="text-xl p-4">
       <h2 className="font-custom text-2xl p-4">About Petful</h2>
       <p className="pl-8 pb-1">Petful was made to help connect folks with pets who are truly in need.</p>
-      <p className="pl-8 pb-1">Too often shelter pets can be neglected because of their breed, age, or any number of factorrs, like allergies, or they have small children andd other pets.</p>
+      <p className="pl-8 pb-1">Too often shelter pets can be neglected because of their breed, age, or any number of factors, like allergies, or they have small children and other pets.</p>
       <p className="pl-8 pb-1">When you use Petful, you may only adopt the eligible dog or cat that has been with us the longest!</p>
       <h2 className="font-custom text-2xl p-4">How To Petful</h2>
       <p className="pl-8 pb-1"> To get started, simply access the adopt page and enter the first and last name on your account. </p>
       <p className="pl-8 pb-1">After submitting, you will know where you are in line or if it is your turn.</p>
-      <p className="pl-8 pb-1">You may adopt up to one cat andd one dog.</p>
+      <p className="pl-8 pb-1">You may adopt up to one cat and one dog.</p>
       <p className="pl-8 pb-1"> When you're up, the portal will unlock and allow you to choose your pet.</p>
     </div>
   )
